e07960003fccc851a2bc1f4d801a112b16a6ff03
Hoda Rashedi <hrashedi1@bcit.ca>
2021-10-24 22:51:16 -0700

    Starting Template COMP1800

---
+++ scripts/firebaseAPI.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+
+var  firebaseConfig = {
+    apiKey: "AIzaSyDHdQ_W1gVDYJp-UouYRpM-D03QALIKIp8",
+    authDomain: "fir-comp1800-644fe.firebaseapp.com",
+    projectId: "fir-comp1800-644fe",
+    storageBucket: "fir-comp1800-644fe.appspot.com",
+    messagingSenderId: "584479157429",
+    appId: "1:584479157429:web:78e882dbf63b9b728222fc"
+  };
+  
+
+  const app = firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
